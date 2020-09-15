de243dd858530a8a094edb19728c7b4d2da0c859
Raul Garcia <garcia@publicplan.de>
2020-09-15 20:29:03 +0200

    03-counter-app

---
+++ 03-counter-app/src/components/FirstApp.js
@@ -0,0 +1,30 @@
+// import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FirstApp = ({ greetting, subtitle }) => {
+
+
+    // const greeting = 'Hola mundo';
+    // const user = {
+    //     name: 'Raul',
+    //     age: 22
+    // }
+    //<> === <Fragment ><Fragment />
+    console.log();
+    return (
+        <>
+            <h1>{ greetting }</h1>
+            {/*<pre>{ JSON.stringify(user, null, 3) }</pre>*/}
+            <p>{ subtitle }</p>
+        </>
+    );
+}
+
+FirstApp.propTypes = {
+    greeting: PropTypes.string.isRequired,
+}
+FirstApp.defaultProps = {
+    subtitle : 'Soy un subtitulo',
+}
+export default FirstApp;
