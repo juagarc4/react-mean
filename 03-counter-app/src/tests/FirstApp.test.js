0d49d83f5180c7a053a0c85cfa0e3f1c8ace2546
Raul Garcia <raulgarciacanet@gmail.com>
2021-04-28 19:09:36 +0200

    Unit and integration tests

--- 03-counter-app/src/tests/FirstApp.test.js
+++ 03-counter-app/src/tests/FirstApp.test.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme'
+// import { render } from '@testing-library/react'
 import FirstApp from '../components/FirstApp'
 
 describe('Test <FirstApp/>', () => {
-  test('should show message "Hello World"', () => {
-    const greeting = 'Hello World'
-    const { getByText } = render(<FirstApp greeting={greeting} />)
+  // test('should show message "Hello World"', () => {
+  //   const greeting = 'Hello World'
+  //   const { getByText } = render(<FirstApp greeting={greeting} />)
 
-    expect(getByText(greeting)).toBeInTheDocument()
+  //   expect(getByText(greeting)).toBeInTheDocument()
+  // })
+  test('should show component <FirstApp /> right', () => {
+    const greeting = 'Hello World'
+    const wrapper = shallow(<FirstApp greeting={greeting} />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+  test('should show subtitle sent by props', () => {
+    const greeting = 'Hello World'
+    const subtitle = 'I am a subtitle'
+    const wrapper = shallow(<FirstApp greeting={greeting} subtitle={subtitle} />)
+
+    const paragraphText = wrapper.find('p').text()
+
+    expect(paragraphText).toBe(subtitle)
   })
 })
