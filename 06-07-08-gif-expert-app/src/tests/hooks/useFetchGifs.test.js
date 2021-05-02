f3640c74f98fcafe5d7814c8c94b542ffebabf89
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-02 15:45:29 +0200

    Testing customHook useFetchGifs. Awaiting for next update

--- 06-07-08-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 06-07-08-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -4,9 +4,22 @@ import { useFetchGifs } from 'hooks/useFetchGifs'
 
 describe('Tests in custom hook useFetchGifs', () => {
   test('Should return initial state', async () => {
-    const { result } = renderHook(() => useFetchGifs('Matrix'))
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs('Matrix')
+    )
     const { gifs, loading } = result.current
+
+    await waitForNextUpdate()
     expect(gifs.length).toBe(0)
     expect(loading).toBe(true)
+  })
+  test('Should return an array of images and loading is false', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs('Matrix')
+    )
+    await waitForNextUpdate()
+    const { gifs, loading } = result.current
+    expect(gifs.length).toBe(10)
+    expect(loading).toBe(false)
   })
 })
