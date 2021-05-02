96d08fc78c5ed4c7f49521924d922011be58eaaf
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-02 17:07:47 +0200

    Advanced Hooks - customHook - useCounter

--- 09-12-advanced-hooks/src/index.js
+++ 09-12-advanced-hooks/src/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 // import { HookApp } from 'HookApp'
-import { CounterApp } from 'components/01-useState/CounterApp'
+// import { CounterApp } from 'components/01-useState/CounterApp'
+import { CounterWithCustomHook } from 'components/01-useState/CounterWithCustomHook'
 
-ReactDOM.render(<CounterApp />, document.getElementById('root'))
+// ReactDOM.render(<HookApp />, document.getElementById('root'))
+// ReactDOM.render(<CounterApp />, document.getElementById('root'))
+ReactDOM.render(<CounterWithCustomHook />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
