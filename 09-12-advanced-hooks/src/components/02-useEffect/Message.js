3eba0e8088a9042b8b5a0ac64cce49a3720ba2d3
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-02 21:12:17 +0200

    Advanced Hooks - useEffect - cautions

--- 09-12-advanced-hooks/src/components/02-useEffect/Message.js
+++ 09-12-advanced-hooks/src/components/02-useEffect/Message.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 })
+  const { x, y } = coords
+
   useEffect(() => {
-    console.log('Component mounted')
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y }
+      setCoords(coords)
+    }
+    window.addEventListener('mousemove', mouseMove)
+
     return () => {
-      console.log('Component unmounted')
+      window.removeEventListener('mousemove', mouseMove)
     }
   }, [])
 
   return (
     <div>
       <h3>You are great</h3>
+      <p>
+        x: {x} y:{y}
+      </p>
     </div>
   )
 }
