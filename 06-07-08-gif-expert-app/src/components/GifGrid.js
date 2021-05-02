ae2a5b71ec5578f78e7bc36d1cbc60abfb835a67
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-02 12:54:30 +0200

    Testing GifGrid mocking response of the api

--- 06-07-08-gif-expert-app/src/components/GifGrid.js
+++ 06-07-08-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { GifGridItem } from 'components/GifGridItem'
 import { useFetchGifs } from 'hooks/useFetchGifs'
+import PropTypes from 'prop-types'
 
 export const GifGrid = ({ category }) => {
   const { gifs, loading } = useFetchGifs(category)
@@ -16,4 +17,8 @@ export const GifGrid = ({ category }) => {
       </div>
     </>
   )
+}
+
+GifGridItem.propTypes = {
+  category: PropTypes.string.isRequired,
 }
---
+++ 06-07-08-gif-expert-app/src/tests/components/GifGrid.test.js
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme'
+import { GifGrid } from 'components/GifGrid'
+import { useFetchGifs } from 'hooks/useFetchGifs'
+
+jest.mock('hooks/useFetchGifs')
+
+describe('Tests in <GifGrid />', () => {
+  const category = 'Dragon Ball'
+
+  test('Should show the component correctly', () => {
+    useFetchGifs.mockReturnValue({ gifs: [], loading: true })
+    const wrapper = shallow(<GifGrid category={category} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+  test('Should show items when they are loaded by useFetchGifs', () => {
+    const gifs = [
+      {
+        id: 'Gif 1',
+        url: 'http://localhost:3000/image.gif',
+        title: 'Gif 1',
+      },
+      {
+        id: 'Gif 2',
+        url: 'http://localhost:3000/image.gif',
+        title: 'Gif 2',
+      },
+    ]
+    useFetchGifs.mockReturnValue({ gifs: gifs, loading: false })
+    const wrapper = shallow(<GifGrid category={category} />)
+    expect(wrapper.find('p').exists()).toBe(false)
+    expect(wrapper.find('GifGridItem').length).toBe(gifs.length)
+  })
+})
