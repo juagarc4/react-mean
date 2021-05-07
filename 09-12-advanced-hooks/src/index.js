2fcf20bc5e4162bd95075ef30dce8e85385c6b83
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-07 19:41:10 +0200

    Advanced Hooks - Form with Custom Hook

--- 09-12-advanced-hooks/src/index.js
+++ 09-12-advanced-hooks/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import reportWebVitals from './reportWebVitals'
 // import { HookApp } from 'HookApp'
 // import { CounterApp } from 'components/01-useState/CounterApp'
 // import { CounterWithCustomHook } from 'components/01-useState/CounterWithCustomHook'
-import { SimpleForm } from 'components/02-useEffect/SimpleForm'
+// import { SimpleForm } from 'components/02-useEffect/SimpleForm'
+import { FormWithCustomHook } from 'components/02-useEffect/FormWithCustomHook'
 
 // ReactDOM.render(<HookApp />, document.getElementById('root'))
 // ReactDOM.render(<CounterApp />, document.getElementById('root'))
 // ReactDOM.render(<CounterWithCustomHook />, document.getElementById('root'))
-ReactDOM.render(<SimpleForm />, document.getElementById('root'))
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'))
