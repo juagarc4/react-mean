3f4a84c709a1ffd3bf5619277f6971f7a89ef525
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-08 19:46:11 +0200

    Advanced Hooks - MultipleCustomHooks - useFetch + useCounter

---
+++ 09-12-advanced-hooks/src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useFetch } from 'hooks/useFetch'
+import { useCounter } from 'hooks/useCounter'
+
+import './effects.css'
+
+export const MultipleCustomHooks = () => {
+  const { counter, increment, decrement } = useCounter()
+
+  const url = `https://www.breakingbadapi.com/api/quotes/${counter}`
+  const { loading, data } = useFetch(url)
+  const { author, quote } = !!data && data[0]
+  const id = !!data && data[0].quote_id
+  return (
+    <div>
+      <h1>Breaking Bad quotes</h1>
+      <hr />
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-justify">
+          <p>
+            {id} - {quote}
+          </p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+      {counter > 1 && (
+        <button className="btn btn-primary" onClick={decrement}>
+          Previous quote
+        </button>
+      )}
+      <button className="btn btn-primary" onClick={increment}>
+        Next quote
+      </button>
+    </div>
+  )
+}
--- 09-12-advanced-hooks/src/index.js
+++ 09-12-advanced-hooks/src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom'
 // import { CounterApp } from 'components/01-useState/CounterApp'
 // import { CounterWithCustomHook } from 'components/01-useState/CounterWithCustomHook'
 // import { SimpleForm } from 'components/02-useEffect/SimpleForm'
-import { FormWithCustomHook } from 'components/02-useEffect/FormWithCustomHook'
+// import { FormWithCustomHook } from 'components/02-useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from 'components/03-examples/MultipleCustomHooks'
 
 // ReactDOM.render(<HookApp />, document.getElementById('root'))
 // ReactDOM.render(<CounterApp />, document.getElementById('root'))
 // ReactDOM.render(<CounterWithCustomHook />, document.getElementById('root'))
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'))
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'))
