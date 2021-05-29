10de9650d57b9147c86d97d84b25d0f7764bb4ac
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-29 16:38:35 +0200

    Advanced Hooks - useContext - Router in React - Link and NavLink

--- 09-12-advanced-hooks/src/components/09-useContext/AppRouter.js
+++ 09-12-advanced-hooks/src/components/09-useContext/AppRouter.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { NavBar } from './NavBar'
+
 import { AboutScreen } from './AboutScreen'
 import { HomeScreen } from './HomeScreen'
 import { LoginScreen } from './LoginScreen'
@@ -7,10 +15,12 @@ export const AppRouter = () => {
   return (
     <Router>
       <div>
+        <NavBar />
         <Switch>
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/about" component={AboutScreen} />
           <Route exact path="/login" component={LoginScreen} />
+          <Redirect to="/" />
         </Switch>
       </div>
     </Router>
