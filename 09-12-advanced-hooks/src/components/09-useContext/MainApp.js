09aaf28ba7c72e9b04c23562764cf502a5936c71
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-29 16:50:57 +0200

    Advanced Hooks - useContext - CreateContext and useContext

--- 09-12-advanced-hooks/src/components/09-useContext/MainApp.js
+++ 09-12-advanced-hooks/src/components/09-useContext/MainApp.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 export const MainApp = () => {
-  return <AppRouter />
+  const user = {
+    id: 1,
+    name: 'Raul Garcia',
+    email: 'email@rgc.com',
+  }
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  )
 }
