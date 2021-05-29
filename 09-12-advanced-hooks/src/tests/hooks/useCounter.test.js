7d7c68a49ffb96ea5258d7ab2e658c4695dc0f3f
Raul Garcia <raulgarciacanet@gmail.com>
2021-05-29 21:41:12 +0200

    Advanced Hooks - Unit and Integration Tests - Test of functions in useCounter

--- 09-12-advanced-hooks/src/tests/hooks/useCounter.test.js
+++ 09-12-advanced-hooks/src/tests/hooks/useCounter.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { useCounter } from 'hooks/useCounter'
 
 describe('Tests in useCounter', () => {
@@ -9,5 +9,43 @@ describe('Tests in useCounter', () => {
     expect(typeof result.current.increment).toBe('function')
     expect(typeof result.current.decrement).toBe('function')
     expect(typeof result.current.reset).toBe('function')
+  })
+
+  test('Should return the given value', () => {
+    const { result } = renderHook(() => useCounter(20))
+    expect(result.current.counter).toBe(20)
+  })
+
+  test('Should increment the counter in 1', () => {
+    const { result } = renderHook(() => useCounter(20))
+    const { increment } = result.current
+
+    act(() => {
+      increment()
+    })
+
+    expect(result.current.counter).toBe(21)
+  })
+  test('Should decrement the counter in 1', () => {
+    const { result } = renderHook(() => useCounter(20))
+    const { decrement } = result.current
+
+    act(() => {
+      decrement()
+    })
+
+    expect(result.current.counter).toBe(19)
+  })
+
+  test('Should reset the counter to the default/given value', () => {
+    const { result } = renderHook(() => useCounter(20))
+    const { increment, reset } = result.current
+
+    act(() => {
+      increment()
+      reset()
+    })
+
+    expect(result.current.counter).toBe(20)
   })
 })
