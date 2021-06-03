2a825f0e1583c0485b23ad720ec3092606cd77d4
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-03 15:45:10 +0200

    Advanced Hooks - Unit and Integration Tests - Tests in Reducer - Part2

--- 09-12-advanced-hooks/src/tests/components/08-useReducer/todoReducer.test.js
+++ 09-12-advanced-hooks/src/tests/components/08-useReducer/todoReducer.test.js
@@ -20,4 +20,26 @@ describe('Tests is todoReducer', () => {
     expect(state.length).toBe(3)
     expect(state).toEqual([...testTodos, newTodo])
   })
+  test('Should remove a todo', () => {
+    const action = {
+      type: 'remove',
+      payload: 1,
+    }
+    const state = todoReducer(testTodos, action)
+    expect(state.length).toBe(1)
+    expect(state).toEqual([testTodos[1]])
+  })
+  test('Should toggle a todo', () => {
+    const action = {
+      type: 'toggle',
+      payload: 1,
+    }
+    const state = todoReducer(testTodos, action)
+    const originalState = testTodos.find((st) => st.id === action.payload)
+    const changedState = state.find((st) => st.id === action.payload)
+    expect(originalState.done).toBe(!changedState.done)
+    // Another type of tests. Less robust in my opinion
+    // expect(state[1].done).toBe(true)
+    // expect(state[0]).toBe(testTodos[0])
+  })
 })
