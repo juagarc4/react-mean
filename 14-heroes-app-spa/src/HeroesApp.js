e91e8b569dfabe0fff5b12de09b05052a66eefa2
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-04 21:52:30 +0200

    14-heroes-spa-app - Initialize the application

---
+++ 14-heroes-app-spa/src/HeroesApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function HeroesApp() {
+  return (
+    <div>
+      <h1>Heroes App</h1>
+    </div>
+  )
+}
---
+++ 14-heroes-app-spa/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HeroesApp } from './HeroesApp'
+ReactDOM.render(<HeroesApp />, document.getElementById('root'))
