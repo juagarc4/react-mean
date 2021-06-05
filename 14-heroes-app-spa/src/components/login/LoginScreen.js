c7ea91ee16d05c2005d0033b653c61abff38fa16
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-05 09:52:09 +0200

    14-heroes-spa-app - History push/replace

--- 14-heroes-app-spa/src/components/login/LoginScreen.js
+++ 14-heroes-app-spa/src/components/login/LoginScreen.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    history.replace('/')
+  }
   return (
-    <div>
+    <div className='container mt-5'>
       <h1>Login Screen</h1>
+      <hr />
+
+      <button className='btn btn-primary' onClick={handleLogin}>
+        Login
+      </button>
     </div>
   )
 }
