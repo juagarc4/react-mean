ce7fef4e3eaae93eab79900930005f667b400789
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-06 20:11:08 +0200

    14-15-16-heroes-app-spa-routes-tests - routes protection - Context and Reducer

--- 14-15-16-heroes-app-spa-routes-tests/src/HeroesApp.js
+++ 14-15-16-heroes-app-spa-routes-tests/src/HeroesApp.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from 'auth/AuthContext'
 import { AppRouter } from './routers/AppRouter'
+import { authReducer } from 'auth/authReducer'
 
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged: false }
+}
 export function HeroesApp() {
-  return <AppRouter />
+  const [user, dispatch] = useReducer(authReducer, {}, init)
+  return (
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <AppRouter />
+    </AuthContext.Provider>
+  )
 }
