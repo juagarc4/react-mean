8335f3677c2f6d1662f16e78dbde02871e6656d9
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-06 12:10:50 +0200

    14-heroes-spa-app - Styling HeroScreen

--- 14-heroes-app-spa/src/components/heroes/HeroScreen.js
+++ 14-heroes-app-spa/src/components/heroes/HeroScreen.js
@@ -2,36 +2,45 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroesById } from '../../selectors/getHeroById'
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
   const { heroId } = useParams()
   const hero = getHeroesById(heroId)
   if (!hero) {
     return <Redirect to='/' />
   }
+
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push('/')
+    } else {
+      history.goBack()
+    }
+  }
   const { superhero, publisher, alter_ego, first_appearance, characters } = hero
 
   return (
-    <div className='card'>
-      <div className='row g-0'>
-        <div className='col-md-4'>
-          <img className='card-img rounded-0' src={`./assets/heroes/${heroId}.jpg`} alt={superhero} />
-        </div>
-        <div className='col-md-6'>
-          <div className='card-body'>
-            <h5 className='card-title'>{superhero}</h5>
-            <h6 className='card-subtitle mb-2 text-muted'>
-              {publisher} - {first_appearance}
-            </h6>
-            <p className='card-text'>
-              <small>{alter_ego}</small>
-            </p>
-            {alter_ego !== characters && (
-              <p className='card-text'>
-                <small>{characters}</small>
-              </p>
-            )}
-          </div>
-        </div>
+    <div className='row mt-5'>
+      <div className='col-md-4'>
+        <img className='img-thumbnail' src={`../assets/heroes/${heroId}.jpg`} alt={superhero} />
+      </div>
+      <div className='col-md-6'>
+        <h3 className='card-title'>{superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <b>Publisher: </b> {publisher}
+          </li>
+          <li className='list-group-item'>
+            <b>First Appearance: </b> {first_appearance}
+          </li>
+          <li className='list-group-item'>
+            <b>Alter Ego: </b> {alter_ego}
+          </li>
+        </ul>
+        <h5>Characters: </h5>
+        <p>{characters}</p>
+        <button onClick={handleReturn} className='btn btn-outline-info'>
+          Return
+        </button>
       </div>
     </div>
   )
