c8aae2b489ff134af406c65006fcd12301b99bf3
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-07 20:36:29 +0200

    14-15-16-heroes-app-spa-routes-tests - routes protection - Private routes

--- 14-15-16-heroes-app-spa-routes-tests/src/routers/AppRouter.js
+++ 14-15-16-heroes-app-spa-routes-tests/src/routers/AppRouter.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { DashboardRoutes } from './DashboardRoutes'
 import { LoginScreen } from '../components/login/LoginScreen'
+import { PrivateRoute } from './PrivateRoute'
+import { AuthContext } from 'auth/AuthContext'
 export function AppRouter() {
+  const { user } = useContext(AuthContext)
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path='/login' component={LoginScreen} />
-          <Route path='/' component={DashboardRoutes} />
+          <Route exact path="/login" component={LoginScreen} />
+          <PrivateRoute
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
