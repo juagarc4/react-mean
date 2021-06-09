b2c11b3a654f2bd5923c0c80b1585a259d1cd7c8
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-09 11:05:00 +0200

    14-15-16-heroes-app-spa-routes-tests - Tests - AppRouter

---
+++ 14-15-16-heroes-app-spa-routes-tests/src/tests/routers/AppRouter.test.js
@@ -0,0 +1,33 @@
+import { AuthContext } from 'auth/AuthContext'
+import { mount } from 'enzyme'
+import { AppRouter } from 'routers/AppRouter'
+
+describe('Tests ins <AppRouter/>', () => {
+  test('Should show login if not authenticate', () => {
+    const contextValue = {
+      dispatch: jest.fn(),
+      user: { logged: false },
+    }
+
+    const wrapper = mount(
+      <AuthContext.Provider value={contextValue}>
+        <AppRouter />
+      </AuthContext.Provider>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('Should show <MarvelScreen/> if authtenticated', () => {
+    const contextValue = {
+      dispatch: jest.fn(),
+      user: { name: 'Raul', logged: true },
+    }
+
+    const wrapper = mount(
+      <AuthContext.Provider value={contextValue}>
+        <AppRouter />
+      </AuthContext.Provider>
+    )
+    expect(wrapper.find('h1').text().trim()).toBe('Marvel Screen')
+  })
+})
