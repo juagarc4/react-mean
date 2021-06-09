e714f59517ea8fc7aa0f4bb0b33b5a7c71fe078b
Raul Garcia <raulgarciacanet@gmail.com>
2021-06-09 13:08:11 +0200

    14-15-16-heroes-app-spa-routes-tests - Tests - HeroScreen with argumernst in URL

--- 14-15-16-heroes-app-spa-routes-tests/src/tests/components/heroes/HeroScreen.test.js
+++ 14-15-16-heroes-app-spa-routes-tests/src/tests/components/heroes/HeroScreen.test.js
@@ -1,6 +1,6 @@
 import { HeroScreen } from 'components/heroes/HeroScreen'
 import { mount } from 'enzyme'
-import { MemoryRouter } from 'react-router-dom'
+import { MemoryRouter, Route } from 'react-router-dom'
 
 describe('Tests in <HeroScreen/>', () => {
   const historyMock = {
@@ -15,5 +15,46 @@ describe('Tests in <HeroScreen/>', () => {
   )
   test('Should show the component <Redirect/> if no hero is present', () => {
     expect(wrapper.find('Redirect').exists()).toBe(true)
+  })
+  test('Should show a hero if the parameter exists and was found ', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+        <Route path="/hero/:heroId" component={HeroScreen} />
+      </MemoryRouter>
+    )
+
+    expect(wrapper.find('.row').exists()).toBe(true)
+  })
+  test('Should return to previous screen with push', () => {
+    const historyMock = {
+      length: 1,
+      goBack: jest.fn(),
+      push: jest.fn(),
+    }
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+        <Route
+          path="/hero/:heroId"
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    )
+    wrapper.find('button').simulate('click')
+    expect(historyMock.push).toHaveBeenCalledWith('/')
+    expect(historyMock.goBack).not.toHaveBeenCalled()
+  })
+  test('Should return to previous screen with goBack', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+        <Route
+          path="/hero/:heroId"
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    )
+    wrapper.find('button').simulate('click')
+    expect(historyMock.push).not.toHaveBeenCalledWith('/')
+    expect(historyMock.goBack).toHaveBeenCalledTimes(1)
   })
 })
