1c8cf583579683ad4fc6cb65b0fc4d4ce9d62cf0
Raul Garcia <raulgarciacanet@gmail.com>
2022-01-12 09:44:10 +0100

    Restarts develop

--- 09-12-advanced-hooks/src/helpers/heavyProcess.js
+++
@@ -1,7 +0,0 @@
-export const heavyProcess = (iterations) => {
-  for (let i = 0; i < iterations; i++) {
-    console.log('Iterating...')
-  }
-
-  return `${iterations} done`
-}
--- 13-custom-hooks-helpers/README.md
+++
@@ -1,5 +0,0 @@
-### Custom Hooks
-
-The repositorx is not in this one, but in a separate place:
-
-https://github.com/juagarc4/my-custom-hooks
